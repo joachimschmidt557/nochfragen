827cc7978cc723b95951bea7115a43056e0efc2d
joachimschmidt557 <joachim.schmidt557@outlook.com>
2025-12-27 10:47:08 +0100

    remove zig backend

--- backend-zig/Context.zig
+++
@@ -1,6 +0,0 @@
-const okredis = @import("okredis");
-const sqlite = @import("sqlite");
-
-redis_client: okredis.BufferedClient,
-db: sqlite.Db,
-root_dir: []const u8,
--- backend-zig/sessions/Options.zig
+++
@@ -1,6 +0,0 @@
-const Cookie = @import("Cookie.zig");
-const SameSiteOption = Cookie.SameSiteOption;
-
-secure: bool = true,
-http_only: bool = false,
-same_site: SameSiteOption = .lax,
